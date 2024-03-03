46bbff63f639fb0fedee1c568af6612b3a7dc57a
Semen Predybailo <s.predybailo@emitech.tech>
2024-03-03 13:41:37 +0300

    Add Voyager and Material3 dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.compose.material3:material3-desktop:1.6.0")
+    implementation("cafe.adriel.voyager:voyager-core:1.1.0-alpha03")
+    implementation("cafe.adriel.voyager:voyager-navigator-desktop:1.1.0-alpha03")
+    implementation("cafe.adriel.voyager:voyager-transitions:1.1.0-alpha03")
 }
 
 compose.desktop {
