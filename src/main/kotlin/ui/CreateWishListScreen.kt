a26da49f086f715c3b7570365551b9695128e2c3
Semen Predybailo <s.predybailo@emitech.tech>
2024-03-04 01:34:04 +0300

    Add feature to create wishlists

--- src/main/kotlin/ui/CreateWishListScreen.kt
+++ src/main/kotlin/ui/CreateWishListScreen.kt
@@ -1,12 +1,58 @@
 package ui
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import viewmodel.WishlistViewModel
 
-class CreateWishlistScreen : Screen {
+class CreateWishlistScreen(private val viewModel: WishlistViewModel) : Screen {
     @Composable
     override fun Content() {
-        Text("Создать вишлист")
+        CreateWishlistScreenUI(viewModel)
     }
 }
+
+@Composable
+fun CreateWishlistScreenUI(viewModel: WishlistViewModel) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Создать вишлист", style = MaterialTheme.typography.titleSmall)
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = viewModel.title.value,
+            onValueChange = { viewModel.title.value = it },
+            label = { Text("Название вишлиста") }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = viewModel.newItemName.value,
+            onValueChange = { viewModel.newItemName.value = it },
+            label = { Text("Добавить пункт") },
+            trailingIcon = {
+                IconButton(onClick = { viewModel.addItem() }) {
+                    Icon(Icons.Default.Add, contentDescription = "Добавить")
+                }
+            }
+        )
+        LazyColumn(modifier = Modifier.fillMaxHeight().weight(1f)) {
+            items(viewModel.items) { item ->
+                Text(item.name)
+            }
+        }
+        Button(
+            onClick = { viewModel.onWishlistCreated() },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("Создать")
+        }
+    }
+}
+
+
--- src/main/kotlin/ui/HomeScreen.kt
+++ src/main/kotlin/ui/HomeScreen.kt
@@ -1,12 +1,45 @@
 package ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import models.WishListModel
 
-class HomeScreen : Screen {
+class HomeScreen(private val wishlists: List<WishListModel>, private val onWishlistSelected: (WishListModel) -> Unit) : Screen {
     @Composable
     override fun Content() {
-        Text("Главный экран")
+        HomeScreenUI(wishlists, onWishlistSelected)
+    }
+}
+
+
+@Composable
+fun HomeScreenUI(wishlists: List<WishListModel>, onWishlistSelected: (WishListModel) -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Ваши вишлисты", style = MaterialTheme.typography.titleSmall)
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(wishlists) { wishlist ->
+                WishlistItemView(wishlist, onWishlistSelected)
+            }
+        }
+    }
+}
+
+@Composable
+fun WishlistItemView(wishlist: WishListModel, onWishlistSelected: (WishListModel) -> Unit) {
+    Card(modifier = Modifier.fillMaxWidth().padding(8.dp).clickable { onWishlistSelected(wishlist) }) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(wishlist.title, style = MaterialTheme.typography.labelMedium)
+            // Дополнительные детали вишлиста
+        }
     }
 }
